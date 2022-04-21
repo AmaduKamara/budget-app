318745dfbc5c9ba65c7e5a22e1bc1697ee60406d
Amkam <amaduskamara@gmail.com>
2022-04-21 18:20:54 +0000

    Add associations to models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,8 @@
 class Category < ApplicationRecord
+  belongs_to :user
+  has_many :category_expenses, dependent: :destroy
+  has_many :expenses, through: :category_expenses
+
+  validates :name, presence: true, length: { in: 1..20 }
+  validates :icon, presence: true, length: { in: 1..300 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :categories
+  has_many :expenses
+
+  validates :name, presence: true, length: { in: 1..15 }
+  validates :surname, presence: true, length: { in: 1..15 }
 end
